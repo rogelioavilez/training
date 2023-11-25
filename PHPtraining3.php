25828d083592143eb79d3f9d79ea3b670032862d
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-24 23:26:29 -0600

    Added new file

---
+++ PHPtraining3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Language Basics</title>
+    <style>
+        body{
+            width: 800px;
+            margin: 0 auto;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    
+    <h1>
+        <?php= echo 'About Me:';?>
+    </h1>
+
+    <h2>
+        <?php= echo 'I\'m Rogelio Avilez and I love to work with PHP!';?>
+    </h2>
+
+    <hr>
+
+    <h1>
+        <?php= echo 'Skills:';?>
+    </h1>
+
+    <?= 'About to develop a website with HTML, CSS, JS and PHP Server Side Programming Language.';?>
+    
+</body>
+</html>
