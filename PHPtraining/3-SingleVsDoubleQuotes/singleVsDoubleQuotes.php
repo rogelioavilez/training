e40f4d75f46588982077d0a6ec192ff53003a754
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-25 11:34:01 -0600

    double and single quotes

---
+++ PHPtraining/3-SingleVsDoubleQuotes/singleVsDoubleQuotes.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Single Vs Double Quotes</title>
+</head>
+<body>
+    
+    <?php
+        $message = "This is PHP Language";
+
+        echo "Printing the message variable using double quotes: $message <br>";
+
+        echo 'Printing the message variable using single quotes: $message';
+    ?>
+
+</body>
+</html>
