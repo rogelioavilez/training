a508406af4a4975096ce10536cb7a5077f365f7a
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-25 11:47:29 -0600

    Added new folder

---
+++ PHPtraining/4-VariableTyping/deliverables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable Typing</title>
+</head>
+<body>
+
+    <h1>Example 1</h1>
+
+    <?php
+        $length = 10;
+        $width = 20;
+        $area = $length * $width;
+        echo "Area: $area"; 
+    ?>
+    
+</body>
+</html>
