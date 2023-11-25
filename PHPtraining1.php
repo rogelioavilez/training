502f1cda98f1d6103d22850dbcdc430a78c36f14
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-24 22:47:19 -0600

    Updated training file learnng Escape characters

--- PHPtraining1.php
+++ PHPtraining1.php
@@ -3,3 +3,24 @@
     echo "White Spaces do not get displayed";
     echo 'This      is    a    Test';
 ?>
+
+<?php
+    /*Escape characters in console
+        \  - This is called as Escaping Character
+        \n - New Line
+        \r - Carriage Return
+        \t - Tab
+        \$ - Dollar Simbol
+        \' - Single quotes
+        \" - Double quotes
+        \\ - Back Slash
+    */
+
+    echo "This isn\'t the way to print";
+
+    echo "\nThis is printed on a new line\n";
+
+    echo "\nWe have a tab \t\t here.\n";
+
+    echo "This is a \"Special character inside PHP!\"";
+?>
