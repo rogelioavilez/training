51c09a4c124be2c807832ee45b494894acc83047
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-24 22:28:31 -0600

    paragraph added

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 </head>
 <body>
     <h1>Welcome</h1>
-    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti repellendus omnis facere similique mollitia at. Aliquid omnis aperiam blanditiis, voluptatem, dolor natus quisquam accusamus, culpa expedita odio quas assumenda officia.</p>
 </body>
 </html>
