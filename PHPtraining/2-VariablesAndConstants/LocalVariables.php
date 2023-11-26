235cdaf9ba0486f934944b6f25dee0b7ac07d224
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-25 21:17:34 -0600

    Added functions into definition of variables

--- PHPtraining/2-VariablesAndConstants/LocalVariables.php
+++ PHPtraining/2-VariablesAndConstants/LocalVariables.php
@@ -10,9 +10,7 @@
     <h1>Example 1</h1>
     <p>Variables defined inside a function and outside</p>
     <?php
-
         $name = "Rogelio";
-
         function print_name(){
             $name = "PHP<br>";
             echo $name;
@@ -20,8 +18,34 @@
 
         print_name();
         echo $name;
-        
     ?>
+    <hr>
+    <h1>Example 2</h1>
+    <p>Variables defined outside a function cannot be detected and show as undefined</p>
+    <?php
+        $name2 = "Alberto";
+        function print_name2(){
+            //Variable is undefined, as its not included inside the function
+            echo $name2;
+        }
+
+        print_name2();
+    ?>
+    <hr>
+    <h1>Example 3</h1>
+    <p>Variables defined outside a function cannot be detected and show as undefined</p>
+    <?php
+    
+        function print_name3(){
+            //Variable is undefined, as its not included inside the function
+            $name3 = "Avilez Garza";
+            echo $name3;
+        }
+
+        print_name3();
+    ?>
+    <hr>
+
     
 </body>
 </html>
