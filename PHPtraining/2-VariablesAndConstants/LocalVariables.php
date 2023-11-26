e39f860ccd6b6f4e8aa474c32f40127945ebac5a
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-25 21:09:06 -0600

    New file Local Variables

---
+++ PHPtraining/2-VariablesAndConstants/LocalVariables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Local Variables</title>
+</head>
+<body>
+
+    <h1>Example 1</h1>
+    <?php
+
+        $name = "Rogelio";
+
+        function print_name(){
+            $name = "PHP<br>";
+            echo $name;
+        }
+
+        print_name();
+        echo $name;
+        
+    ?>
+    
+</body>
+</html>
