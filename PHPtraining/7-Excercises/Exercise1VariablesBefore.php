cb82b9890bf5c39113385b8766a571b8b9e80cba
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-26 12:09:35 -0600

    excersice

--- PHPtraining/7-Excercises/Exercise1VariablesBefore.php
+++ PHPtraining/7-Excercises/Exercise1VariablesBefore.php
@@ -10,10 +10,59 @@
     <h1>Exercise 1: Create and Display Variables</h1>
 
     <h2>String Variable:</h2>
+    <pre>
+        <code>
+            $varString = "First String Variable";
+
+            echo "String Variable = " . $varString;
+        </code>
+    </pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        $varString = "First String Variable";
+
+        echo "String Variable = " . $varString;
+    ?>
+    <hr>
 
     <h2>Integer Variable:</h2>
+    <pre>
+        <code>
+            $varInteger = 10;
+
+            echo "Integer Variable = " . $varInteger;
+        </code>
+    </pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        $varInteger = 10;
+
+        echo "Integer Variable = " . $varInteger;
+    ?>
+    <hr>
 
     <h2>Print Variable from Function:</h2>
+    <pre>
+        <code>
+            function print_variable(){<br>
+                $varInsideFunction = "I'm Trapped Here";<br>
+                echo $varInsideFunction;<br>
+            }<br><br>
+            
+            print_variable();
+        </code>
+    </pre>
+    <h3><i>Result:</i></h3>
+    <?php
+
+        function print_variable(){
+            $varInsideFunction = "I'm Trapped Here";
+            echo $varInsideFunction;
+        }
+        
+        print_variable();
+    ?>
+    <hr>
 
     <h2>Static Variable:</h2>
 
