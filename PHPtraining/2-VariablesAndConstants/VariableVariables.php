cbb654b7d708700bd1cbf7ea7829fd34e9b6567c
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-25 22:54:16 -0600

    New file

---
+++ PHPtraining/2-VariablesAndConstants/VariableVariables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable Variables</title>
+</head>
+<body>
+    <h1>Example 1</h1>
+    <p>Description</p>
+
+    <?php
+        //Male is a variale
+        $male = "Rogelio, Avilez";
+        //Male is holding a variable name
+        $gender = "Male";
+
+        echo $male;
+        echo $$gender; //$($gender) ==> $(male)
+    ?>
+</body>
+</html>
