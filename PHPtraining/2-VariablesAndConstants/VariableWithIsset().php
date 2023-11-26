128a0fd51fa3224fafc161a76fb24611ce3964ed
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-25 23:13:12 -0600

    New file isset()

---
+++ PHPtraining/2-VariablesAndConstants/VariableWithIsset().php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable with isset()</title>
+</head>
+<body>
+    <h1>Variable with isset()</h1>
+    <br>
+    <h2>Example 1</h2>
+</body>
+</html>
