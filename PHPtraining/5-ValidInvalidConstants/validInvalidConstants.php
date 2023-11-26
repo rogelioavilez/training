ebc10d515aaaa40e9d44e85fcdecb2d55e61e1d1
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-26 11:08:15 -0600

    New file

---
+++ PHPtraining/5-ValidInvalidConstants/validInvalidConstants.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valid Invalid Constants</title>
+</head>
+<body>
+    <h1>Valid Invalid Constants</h1>
+    <h3>Good Practice 1:</h3>
+    <p>Always use UPPERCASE to define constants.</p>
+
+    <pre>
+        <code>
+            define (LANGUAGE, "PHP");
+            define (VERSION, 7.3);
+
+            echo "Language" . LANGUAGE . "Version: " . VERSION;
+        </code>
+    </pre>
+
+    <?php
+        define (LANGUAGE, "PHP");
+        define (VERSION, 7.3);
+
+        echo "Language" . LANGUAGE . "Version: " . VERSION;
+
+    ?>
+    
+</body>
+</html>
