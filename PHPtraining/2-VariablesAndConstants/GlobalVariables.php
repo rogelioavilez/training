35c97613d19dd53a50d55e98212ab4006d8b9310
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-25 21:22:38 -0600

    Added global variables file

---
+++ PHPtraining/2-VariablesAndConstants/GlobalVariables.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Global Variables</title>
+</head>
+<body>
+    <h1>Example 1</h1>
+
+    <?php
+        echo 'Test1';
+    ?>
+    
+</body>
+</html>
