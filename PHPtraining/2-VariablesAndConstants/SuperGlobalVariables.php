1f5635e28ce56a277b5ab6f9ae8b4c5622f48f8a
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-25 22:32:56 -0600

    Update doc superglobals

--- PHPtraining/2-VariablesAndConstants/SuperGlobalVariables.php
+++ PHPtraining/2-VariablesAndConstants/SuperGlobalVariables.php
@@ -6,7 +6,15 @@
     <title>Super Global Variables</title>
 </head>
 <body>
-    <h1>Super Global Variables</h1>
-    
+    <h1>Example 1</h1>
+    <p>Description</p>
+
+    <?php
+        global $message;
+        $message = "Welcome to PHP!";
+
+        echo $GLOBALS['message'] . "<br>";
+        
+    ?>
 </body>
 </html>
