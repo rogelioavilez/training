1647b8b374a5146643101f7d02de63156563fcd8
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-26 10:38:04 -0600

    new file v vs c

---
+++ PHPtraining/4-VariablesVsConstants/variablesVsConstants.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables Vs Constants</title>
+</head>
+<body>
+
+    <h1>Variables Vs Constants</h1>
+
+    <h3>Constants characteristics</h3>
+    <pre>
+        <code>
+            1) No need to use $ to use constant
+            2) Constant are defined only via define method
+            3) Value can be assigned only once
+            4) Constant has global scope. Can access anywhere.
+            5) Constant can be used to access the constants
+
+            $name = "Rogelio, Avilez";
+            define("name", "John, Smith");
+
+            echo $name . name;
+
+        </code>
+    </pre>
+
+    <h3><i>Results:</i></h3>
+       
+    <?php 
+        //define ( NAME OF THE CONSTANT, VALUE OF THE CONSTANT);
+
+        $name = "Rogelio, Avilez";
+        define("name", "John, Smith");
+
+        echo $name . name;
+        
+    ?>
+    
+</body>
+</html>
