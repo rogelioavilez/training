932ccf4f57f8611277b76cd6ac141624cc537690
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-25 21:41:29 -0600

    New file Static Variables

---
+++ PHPtraining/2-VariablesAndConstants/StaticVariables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Static Variables</title>
+</head>
+<body>
+    <h1>Example 1</h1>
+    <p>Description</p>
+    
+    <?php
+        function counter(){
+            $count = 1;
+            echo $count . "<br>";
+            $count+= 1;
+        }
+    ?>
+
+</body>
+</html>
