7bc77d1b60f0f454b2ca2626e66599c0c23b2214
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-25 23:36:41 -0600

    new file added

---
+++ PHPtraining/2-VariablesAndConstants/VariableConcepts.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concepts</title>
+</head>
+<body>
+
+    <h1>Variable Concepts</h1>
+    <hr>
+    <!-- Define a Variable -->
+    <p>Define a variable: $name = "Rogelio, Avilez";</p>
+    <?php $name = "Rogelio, Avilez";  ?>
+    
+</body>
+</html>
