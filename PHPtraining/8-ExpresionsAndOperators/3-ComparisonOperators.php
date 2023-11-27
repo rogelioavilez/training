59da144b9297035d30e0f0bfed53a4dcf93b9639
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-27 15:02:02 -0600

    1

--- PHPtraining/8-ExpresionsAndOperators/3-ComparisonOperators.php
+++ PHPtraining/8-ExpresionsAndOperators/3-ComparisonOperators.php
@@ -7,19 +7,33 @@
 </head>
 <body>
     <h1>Comparison Operator</h1>
+    <!--
+        ==  Equal
+        === Data/Value Identical
+        !=  /  < >   /   !==    Not Equal To
+        > Greater than
+        < Less than
+        >= Greater Than or Equal
+        <= Less than or Equal
+    -->
 
     <h3></h3>
-    <p></p>
+    <p>Use Equal to == Operator to check the PHP Version</p>
 
     <pre>
         <code>
+        $php_version = 7.5;
+        $php_system_version = PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION;
+        echo ($php_version == $php_system_version)? "Version Match" : "Version don't match";
         </code>
     </pre>
 
     <h3><i>Result:</i></h3>
 
     <?php
-        
+        $php_version = 7.5;
+        $php_system_version = PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION;
+        echo ($php_version == $php_system_version)? "Version Match" : "Version don't match";
     ?>
 
 </body>
