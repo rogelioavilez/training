a5274eb2ad945f96865959d83654110f3cad389e
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-27 16:07:25 -0600

    1

--- PHPtraining/8-ExpresionsAndOperators/4-LogicalOperators.php
+++ PHPtraining/8-ExpresionsAndOperators/4-LogicalOperators.php
@@ -7,6 +7,51 @@
 </head>
 <body>
     <h1>Logical Operators</h1>
-    
+    <p>
+        <pre>
+        AND - Both should be true.
+        OR - Anyone can be true.
+        && - Both should be true.
+        || - Anyone can be true.
+        ! - If not.
+        </pre>
+    </p>
+
+<div>
+    <h3></h3>
+    <p>Great Hello, Name if the user is logged in and has permission.</p>
+    <pre>
+        <code>
+        $permission = true;
+        $loggedIn = true;
+        $user = "Rogelio Avilez";
+        echo ($permission and $loggedIn) ? "Hello " . $user : "Hello Guest";
+        </code>
+    </pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        $permission = true;
+        $loggedIn = true;
+        $user = "Rogelio Avilez";
+        echo ($permission and $loggedIn) ? "Hello " . $user : "Hello Guest";
+    ?>
+</div>
+<hr>
+
+<div>
+    <h3></h3>
+    <p></p>
+    <pre>
+        <code>
+
+        </code>
+    </pre>
+    <h3><i>Result:</i></h3>
+    <?php
+
+    ?>
+</div>
+<hr>
+
 </body>
 </html>
