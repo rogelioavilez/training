abcdac3b4d6dc0f29e3850e574ea2829df0dfcf0
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-27 08:54:10 -0600

    Test start

---
+++ PHPtraining/7-Excercises/test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <h1>Test</h1>
+
+    <h3>1) Define and Assign $firstname and $lastname variable with your name.</h3>
+
+    <pre>
+        <code>
+            $firstname = "Rogelio";
+            $lastname = "Avilez Garza";
+        </code>
+    </pre>
+    <h3>Result:</h3>
+    <?php
+        $firstname = "Rogelio";
+        $lastname = "Avilez Garza";
+    ?>
+    
+</body>
+</html>
