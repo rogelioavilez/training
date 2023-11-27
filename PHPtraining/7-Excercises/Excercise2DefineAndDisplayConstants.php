eab7b9d81a6334fe036966e738f8688f14be8a83
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-27 08:39:47 -0600

    New document

---
+++ PHPtraining/7-Excercises/Excercise2DefineAndDisplayConstants.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Describe and Display Constants</title>
+</head>
+<body>
+    <h1>Describe and Display Constants</h1>
+
+    <h3>String Constant</h3>
+    <pre>
+        <code>
+            define ("COWORKER_ID", 001);
+            echo "Coworker ID: " . COWORKER_ID;
+        </code>
+    </pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        define ("COWORKER_ID", 001);
+        echo "Coworker ID: " . COWORKER_ID;
+    ?>
+    <hr>
+
+    <h3>Integer Constant</h3>
+    <pre>
+        <code>
+
+        </code>
+    </pre>
+    <h3><i>Results:</i></h3>
+    <?php
+
+    ?>
+    <hr>
+
+    <h3>Print Constant From Function</h3>
+    <pre>
+        <code>
+
+        </code>
+    </pre>
+    <h3><i>Results:</i></h3>
+    <?php
+
+    ?>
+    <hr>
+
+    <h3>Display Magic Constant</h3>
+    <pre>
+        <code>
+
+        </code>
+    </pre>
+    <h3><i>Results:</i></h3>
+    <?php
+
+    ?>
+    <hr>
+    
+</body>
+</html>
