65da55cb67bd5970b28d32f4bd98f4e707d1ed0b
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-27 08:44:17 -0600

    update excercise

--- PHPtraining/7-Excercises/Excercise2DefineAndDisplayConstants.php
+++ PHPtraining/7-Excercises/Excercise2DefineAndDisplayConstants.php
@@ -11,38 +11,44 @@
     <h3>String Constant</h3>
     <pre>
         <code>
-            define ("COWORKER_ID", 001);
-            echo "Coworker ID: " . COWORKER_ID;
+            define ("COWORKER_NAME", "Rogelio, Avilez");
+            echo "Coworker Name: " . COWORKER_NAME;
         </code>
     </pre>
     <h3><i>Result:</i></h3>
     <?php
-        define ("COWORKER_ID", 001);
-        echo "Coworker ID: " . COWORKER_ID;
+        define ("COWORKER_NAME", "Rogelio, Avilez");
+        echo "Coworker Name: " . COWORKER_NAME;
     ?>
     <hr>
 
     <h3>Integer Constant</h3>
     <pre>
         <code>
-
+            define ("COWORKER_ID", 1001);
+            echo "Coworker ID: " . COWORKER_ID;
         </code>
     </pre>
     <h3><i>Results:</i></h3>
     <?php
-
+        define ("COWORKER_ID", 1001);
+        echo "Coworker ID: " . COWORKER_ID;
     ?>
     <hr>
 
     <h3>Print Constant From Function</h3>
     <pre>
         <code>
-
+            
         </code>
     </pre>
     <h3><i>Results:</i></h3>
     <?php
+        function print_constant(){
+                echo "Coworker: " . COWORKER_NAME . " Has ID # " . COWORKER_ID;
+        }
 
+        print_constant();
     ?>
     <hr>
 
