a0016b6f868e46f92b72025e863b7a5acbfa2d8b
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-27 15:53:44 -0600

    5

--- PHPtraining/8-ExpresionsAndOperators/3-ComparisonOperators.php
+++ PHPtraining/8-ExpresionsAndOperators/3-ComparisonOperators.php
@@ -106,7 +106,7 @@
     ?>
 </div>
 <hr>
-<!-- !=  /  < >   /   !==    Not Equal To -->
+<!-- > Greater Than -->
 <div>
     <h3>Greater than ></h3>
     <p>Find the senior citizen whose age is greater than 60 using greater than operator</p>
@@ -149,7 +149,51 @@
         echo ($citizenAge3 > $ageLimit) ? $citizen3 . " is a Senior Citizen" : $citizen3 . " is not a Senior Citizen";
     ?>
 </div>
-    
+<hr>
+<!-- < Less Than -->
+<div>
+    <h3>Less than < </h3>
+    <p>Find the kids that are getting pizza for free</p>
+
+    <pre>
+        <code>
+        $citizen1 = "Jhon";
+        $citizen1 = "Mark";
+        $citizen1 = "Luke";
+
+        $ageLimit = 10;
+        $citizenAge1 = 40;
+        $citizenAge2 = 64;
+        $citizenAge3 = 80;
+
+        echo ($citizenAge1 > $ageLimit) ? $citizen1 . " is a Senior Citizen" : $citizen1 . " is not a Senior Citizen";
+        echo "<\br>";
+        echo ($citizenAge2 > $ageLimit) ? $citizen2 . " is a Senior Citizen" : $citizen2 . " is not a Senior Citizen";
+        echo "<\br>";
+        echo ($citizenAge3 > $ageLimit) ? $citizen3 . " is a Senior Citizen" : $citizen3 . " is not a Senior Citizen";
+        </code>
+    </pre>
+
+    <h3><i>Result:</i></h3>
+
+    <?php
+        $name1 = "Jhonny";
+        $name2 = "Markus";
+        $name3 = "Lukas";
+
+        $ageLimit = 10;
+        $nameAge1 = 4;
+        $nameAge2 = 64;
+        $nameAge3 = 8;
+
+        echo ($nameAge1 < $ageLimit) ? $name1 . " gets to eat pizza for free" : $name1 . " doesn't get pizza";
+        echo "<br>";
+        echo ($nameAge2 < $ageLimit) ? $name2 . " gets to eat pizza for free" : $name2 . " doesn't get pizza";
+        echo "<br>";
+        echo ($nameAge3 < $ageLimit) ? $name3 . " gets to eat pizza for free" : $name3 . " doesn't get pizza";        
+    ?>
+</div>
+<hr>
 
 </body>
 </html>
