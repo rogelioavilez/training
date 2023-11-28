7791010d78832bf3aed26fdcf4ce82fff5cb221d
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-27 22:37:22 -0600

    1

--- PHPtraining/8-ExpresionsAndOperators/6-IncrementDecrementOperators.php
+++ PHPtraining/8-ExpresionsAndOperators/6-IncrementDecrementOperators.php
@@ -7,7 +7,40 @@
 </head>
 <body>
     <h1>Increment Decrement Operators</h1>
+    <p><pre>
+        ++  Incremental by 1
+        --  Decremental by 1
+    </pre></p>
 
-    
+<div>
+    <h3>++ Increment</h3>
+    <pre><code>
+        $counter = 1;
+        $counter ++;
+
+        echo "Added 1 to Counter " . $counter;
+    </code></pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        $counter = 1;
+        $counter ++;
+
+        echo "Added 1 to Counter " . $counter;
+    ?>
+    <hr>
+</div>
+
+<div>
+    <h3>-- Decrement</h3>
+    <pre><code>
+
+    </code></pre>
+    <h3><i>Result:</i></h3>
+    <?php
+
+    ?>
+    <hr>
+</div>
+
 </body>
 </html>
