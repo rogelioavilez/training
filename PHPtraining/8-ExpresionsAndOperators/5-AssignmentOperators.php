2801f5134c977cebb60880c6df42d7f8ee3013cc
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-27 22:16:25 -0600

    1.2

--- PHPtraining/8-ExpresionsAndOperators/5-AssignmentOperators.php
+++ PHPtraining/8-ExpresionsAndOperators/5-AssignmentOperators.php
@@ -9,34 +9,46 @@
     <h1>Assignment Operators</h1>
     <p><pre>
     =   Assigns Values
-    +=  Add Assignment Operator
+    .=  Add String Assignment Operator
+    +=  Add Integer Assignment Operator
     -=  Subtract Assignment Operator
     *=  Multiply Assignment Operator
     /=  Divide Assignment Operator
     %=  Modulus Assignment Operator
     </pre></p>
 
-    <?php
-    $marks = 10;
-    $name = "Rogelio, Avilez";
-    ?>
 <div>
-    <h3>Example 1</h3>
-    <p>Use += Operator</p>
+    <h3>Use += Operator</h3>
 
     <pre><code>
+        $marks = 10;
+        $name = "Rogelio";
+
+        $name .= ", ";
+        $name .= "Avilez";
+        echo "My Full Name is: " .  $name;
+
+
         $counter = 1;
         $counter += 1;
         $counter += $counter;
-        echo "$counter:" . $counter;
+        echo "Variable counter is equal to: " . $counter;
 
         echo "<br>";
 
         $marks += 10;
-        echo "$marks:" . $marks;
+        echo "Variable marks is equal to: " . $marks;
     </code></pre>
     <h3><i>Result:</i></h3>
     <?php
+        $marks = 10;
+        $name = "Rogelio";
+
+        $name .= ", ";
+        $name .= "Avilez";
+        echo "My Full Name is: " .  $name;
+
+
         $counter = 1;
         $counter += 1;
         $counter += $counter;
@@ -47,6 +59,20 @@
         $marks += 10;
         echo "Variable marks is equal to: " . $marks;
     ?>
+    <hr>
+</div>
+
+<div>
+    <h3>Use -= Operator</h3>
+
+    <pre><code>
+        
+    </code></pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        
+    ?>
+    <hr>
 </div>
 
 
