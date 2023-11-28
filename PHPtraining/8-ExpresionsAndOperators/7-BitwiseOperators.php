3bb60f29a0ffa18987213683e32ef4ce74d13047
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-27 22:57:14 -0600

    1

--- PHPtraining/8-ExpresionsAndOperators/7-BitwiseOperators.php
+++ PHPtraining/8-ExpresionsAndOperators/7-BitwiseOperators.php
@@ -7,6 +7,42 @@
 </head>
 <body>
     <h1>Bitwise Operators</h1>
+    <p><pre>
+        &   AND
+        |   OR
+        ^   XOR
+        -   NOT
+
+
+        How Bits Work
+
+        0   bits    0000
+        1   bits    0001
+        2   bits    0010
+        3   bits    0011
+        4   bits    0100
+        5   bits    0101
+
+        1 - 1 = 1
+        0 - 0 = 0
+        0 - 1 = 0
+        1 - 0 = 0
+
+    </pre></p>
+
+<div>
+    <h3></h3>
+    <pre><code>
+
+    </code></pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        $first = 0; //0000
+        $second = 1; //0001
+
+        echo $first & $second;
+    ?>
+</div>
     
 </body>
 </html>
