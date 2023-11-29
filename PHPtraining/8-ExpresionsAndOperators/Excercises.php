948d7992154748a097223cc28ae36618f28640ea
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-28 21:32:14 -0600

    1

--- PHPtraining/8-ExpresionsAndOperators/Excercises.php
+++ PHPtraining/8-ExpresionsAndOperators/Excercises.php
@@ -13,12 +13,26 @@
         <p>Add 2 numbers using Arithmeical operators</p>
         <pre><code>
             $value = 2 + 2;
-            echo "Add 2 + 2 = " . $value;
+            echo "2 + 2 = " . $value;
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
             $value = 2 + 2;
-            echo "Add 2 + 2 = " . $value;
+            echo "2 + 2 = " . $value;
+        ?>
+    </div>
+
+    <div>
+        <h3>Excercise 2</h3>
+        <p>Find Student Passed or Failed using Conditional Operator</p>
+        <pre><code>
+            $passed(true) ? "Pass" : "Fail" ;
+            echo "Condition is: " . $passed;
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            $passed(true) ? "Pass" : "Fail" ;
+            echo "Condition is: " . $passed;
         ?>
     </div>
 </body>
