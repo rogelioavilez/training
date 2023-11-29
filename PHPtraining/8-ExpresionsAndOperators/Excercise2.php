4d43b8fe9bc208410eec4c11d08ab6c5f0b38f34
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-28 22:08:28 -0600

    new file

---
+++ PHPtraining/8-ExpresionsAndOperators/Excercise2.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Excercises 2</title>
+</head>
+<body>
+    <h1>Excercises 2</h1>
+
+    <div>
+        <h3>Excercise 1</h3>
+        <p></p>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            
+        ?>
+    </div>
+<hr>
+    <div>
+        <h3>Excercise 2</h3>
+        <p></p>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+
+        ?>
+    </div>
+
+<hr>
+    <div>
+        <h3>Excercise 3</h3>
+        <p></p>
+        <pre><code>
+              
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            
+        ?>
+    </div>
+
+    <hr>
+    <div>
+        <h3>Excercise 4</h3>
+        <p></p>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            
+        ?>
+    </div>
+
+    <hr>
+    <div>
+        <h3>Excercise 5</h3>
+        <p></p>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            
+        ?>
+    </div>
+</body>
+</html>
