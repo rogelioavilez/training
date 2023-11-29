792a4139eac0caaa8509188b16fcd10eafc608f5
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-28 20:36:29 -0600

    new file

---
+++ PHPtraining/8-ExpresionsAndOperators/9-ErrorControlOperators.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error Control Operators</title>
+</head>
+<body>
+    <h1>Error Control Operators</h1>
+
+    <div>
+        <h3>Backtick symbol ``</h3>
+        <p>Left side to number one on keyboard</p>
+        <pre><code>
+            //WINDOWS
+            echo `dir *.php`;
+            echo "<\br><\br>";
+            // MAC
+            echo `ls *.php`;
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            //WINDOWS
+            echo `dir *.php`;
+            echo "<br><br>";
+            // MAC
+            echo `ls *.php`;
+        ?>
+    </div>
+</body>
+</html>
