0d1885bc69ea88c95c49e4f2a440390e15f2fbc2
rogelioavilez <rogelioavilez@oneqrid.com>
2023-11-28 20:30:30 -0600

    1

--- PHPtraining/8-ExpresionsAndOperators/8-ExecutionOperators.php
+++ PHPtraining/8-ExpresionsAndOperators/8-ExecutionOperators.php
@@ -7,5 +7,18 @@
 </head>
 <body>
     <h1>Execution Operators</h1>
+
+    <div>
+        <h3>Backtick symbol ``</h3>
+        <p>Left side to number one on keyboard</p>
+        <pre><code>
+
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            echo `dir *.php`;
+            echo `ls *.php`;
+        ?>
+    </div>
 </body>
 </html>
