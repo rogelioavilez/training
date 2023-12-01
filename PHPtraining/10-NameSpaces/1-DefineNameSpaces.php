4b99902217034f61aaba2c3c2693880fb5c9dd08
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-01 14:41:19 -0600

    newfile

--- PHPtraining/10-NameSpaces/1-DefineNameSpaces.php
+++ PHPtraining/10-NameSpaces/1-DefineNameSpaces.php
@@ -7,5 +7,30 @@
 </head>
 <body>
     <h1>Define Name Spaces</h1>
+
+    <pre>
+        * Namespaces are case insensitive.
+        * Namespaces are used to avoid name collition between other libraries.
+        * Namespace is the keyboard used to define namespace.
+        * Only Classes, Interface, Functions and Constants should be defined in Namespaces.
+        * Namespace should always be first in line.
+        * include is used to include any file into another file.
+        * Use backslash to refer to subdirectories or file name.
+    </pre>
+
+    <h3>Namespaces are case insensitive</h3>
+    <pre>
+        <code>
+        namespace myconstants;
+
+        const FILE_NAME = "NamespaceConstants.php";
+        </code>
+    </pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        namespace myconstants;
+
+        const FILE_NAME = "NamespaceConstants.php";
+    ?>
 </body>
 </html>
