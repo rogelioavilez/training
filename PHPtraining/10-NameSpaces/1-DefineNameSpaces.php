1d3926eaa2de350f8640888f149b6b791b99a738
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-01 14:01:13 -0600

    new file

---
+++ PHPtraining/10-NameSpaces/1-DefineNameSpaces.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Define Name Spaces</title>
+</head>
+<body>
+    <h1>Define Name Spaces</h1>
+</body>
+</html>
