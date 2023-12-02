e3f89bf487fcca2005e2a11b2d8186745f4bfa96
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-02 17:00:51 -0600

    5

--- PHPtraining/11-ControlStructure/IfElseStatement.php
+++ PHPtraining/11-ControlStructure/IfElseStatement.php
@@ -105,5 +105,33 @@
             $some_boolean_condition = FALSE; if($some_boolean_condition)echo "<h4>This is a If block condition written in one line</h4><p>This is a True Condition</p><br>"; else echo "<h4>This is an Else block condition written in one line</h4><p>This is a False Condition</p>";
         ?>
     </div>
+
+    <hr>
+    <div>
+        <h3>Recommended Practice</h3>
+        <pre><code>
+            Always use brackets to have better visual organization.
+
+            $some_boolean_condition = FALSE;
+            if ($some_boolean_condition) {
+                echo "<\h4>This is a If block condition written in one line</\h4>
+                <\p>This is a True Condition</\p><\br>"; 
+            } else {
+                echo "<\h4>This is an Else block condition written in one line</\h4>
+                <\p>This is a False Condition</\p>";
+            }
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            $some_boolean_condition = FALSE;
+            if($some_boolean_condition){
+                echo "<h4>This is a If block condition written in one line</h4>
+                <p>This is a True Condition</p><br>"; 
+            }else { 
+                echo "<h4>This is an Else block condition written in one line</h4>
+                <p>This is a False Condition</p>";
+            }
+        ?>
+    </div>
 </body>
 </html>
