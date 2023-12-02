3e5a3c3e715d87648986fdcbf1530111caa4247d
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-02 16:14:58 -0600

    new file

--- PHPtraining/11-ControlStructure/IfElseStatement.php
+++ PHPtraining/11-ControlStructure/IfElseStatement.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Control Structures</title>
+    <title>If, Else, Statement</title>
 </head>
 <body>
-    <h1>Control Structures</h1>
+    <h1>If, Else, Statement</h1>
 </body>
 </html>
