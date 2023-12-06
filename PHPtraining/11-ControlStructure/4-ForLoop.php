10e47e03a968552ed1690f0cdae22ccfbdc75359
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-05 19:45:18 -0600

    new file

---
+++ PHPtraining/11-ControlStructure/4-ForLoop.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For Loop</title>
+</head>
+<body>
+    <h1>For Loops</h1>
+
+    <hr>
+    <div>
+        <pre><code>
+
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+        
+        ?>
+    </div>
+    
+</body>
+</html>
