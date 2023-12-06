b6a62e393a042139f14ad104610c06bebb3da977
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-05 20:28:58 -0600

    new file

---
+++ PHPtraining/11-ControlStructure/6-DoWhileLoop.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Do While Loop</title>
+</head>
+<body>
+    <h1>Do While Loops</h1>
+    <pre>
+    do while (Condition) {
+            ** Code to be executed **
+        }
+    </pre>
+    <hr>
+    <div>
+        <h3></h3>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+
+        ?>
+    </div>
+    
+</body>
+</html>
