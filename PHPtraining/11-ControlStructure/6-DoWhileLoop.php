b6ea20dc680a4ff7e85f1b835fb57f3396a40050
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-05 20:33:25 -0600

    1

--- PHPtraining/11-ControlStructure/6-DoWhileLoop.php
+++ PHPtraining/11-ControlStructure/6-DoWhileLoop.php
@@ -8,19 +8,38 @@
 <body>
     <h1>Do While Loops</h1>
     <pre>
-    do while (Condition) {
+        do{
             ** Code to be executed **
-        }
+        } 
+        while (Condition);
     </pre>
     <hr>
     <div>
-        <h3></h3>
+        <h3>Print Square of a number maximum until 100 using Do While Loop</h3>
         <pre><code>
+            $max_value = 100;
+            $value1 = 2;
+
+            do{
+                echo "Square: " . $value1 . PHP_EOL;
+                $value1 *= 2;
+            } 
+            while ($value1 <= $max_value);
             
+            //Dont forget semicolon after While Condition
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
 
+            $max_value = 100;
+            $value1 = 2;
+
+            do{
+                echo "Square: " . $value1 . PHP_EOL;
+                $value1 *= 2;
+            } 
+            while ($value1 <= $max_value);
+            //Dont forget semicolon after While Condition
         ?>
     </div>
     
