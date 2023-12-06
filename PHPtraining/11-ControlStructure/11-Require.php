ef8321b2779a4d3f43f596345876624fbd891b06
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-05 21:52:54 -0600

    newfile

---
+++ PHPtraining/11-ControlStructure/11-Require.php
@@ -0,0 +1,21 @@
+
+<?php
+
+    include "10.1-header.php";
+?>
+
+<body>
+
+    <?php
+        include "10.2-menu.php";
+        include "10.2-submenu.php";
+
+    ?>
+
+    <?php
+        include "10.3-footer.php";
+    ?>
+
+</body>
+
+</html>
