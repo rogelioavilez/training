b1cc502d26f000c7bd59c652c19049372dbbdf2f
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-05 21:11:45 -0600

    1

--- PHPtraining/11-ControlStructure/8-Continue.php
+++ PHPtraining/11-ControlStructure/8-Continue.php
@@ -7,14 +7,45 @@
 </head>
 <body>
     <h1>Continue</h1>
-
+    <pre>
+        * Continue will skip the next line and go to next loop.
+    </pre>
     <hr>
+    <h3>Example 1: Print only Even Numbers in result</h3>
     <pre><code>
+        $value1 = 1;
+        while (true){
+            $result = $value1 % 2;
 
+            if($result ==1){
+                continue;
+            }
+
+            if ($value1 > 10){
+                break;
+            }
+
+            echo "Even Numbers: " . $value1 . "<br>";
+            $value1++;
+        }
     </code></pre>
     <h3><i>Result:</i></h3>
     <?php
-        
+        $value1 = 1;
+        while (true){
+            $result = $value1 % 2;
+
+            if($result ==1){
+                continue;
+            }
+
+            if ($value1 > 10){
+                break;
+            }
+
+            echo "Even Numbers: " . $value1 . "<br>";
+            $value1++;
+        }
     ?>
     
 </body>
