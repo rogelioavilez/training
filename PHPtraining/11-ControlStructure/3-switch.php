b447a12ab79fec79188ca9997cbcdd3ac5c2a0a0
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-05 19:31:59 -0600

    2.1

--- PHPtraining/11-ControlStructure/3-switch.php
+++ PHPtraining/11-ControlStructure/3-switch.php
@@ -32,6 +32,30 @@
                 echo 'Invalid Input';
 
         }
+
+        $input_number = 9;
+        $result = $input_number % 2;
+
+        if ( $result ){
+
+            echo 'Odd Number in If Statement';
+
+        } else if (!$result){
+
+            echo 'Even Number in If Statement';
+
+        } switch ($result) {
+
+            case $input_number > 10:
+                echo '$input_number is greater than 10';
+                break;
+            case $input_number == 10:
+                echo '$input_number is equal to 10';
+                break;
+            default:
+                echo '$input_number is less than 10';
+
+        }
     </code></pre>
     <h3><i>Result:</i></h3>
     <?php
