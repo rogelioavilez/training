2ee71208a2802a03ddf8b8aa2f22c2a1d9d07a50
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-05 21:27:52 -0600

    1

--- PHPtraining/11-ControlStructure/9-Return.php
+++ PHPtraining/11-ControlStructure/9-Return.php
@@ -8,16 +8,63 @@
 <body>
     <h1>Return</h1>
     <pre>
-        * 
+        * Return will help exclude the next lines if added a condition.
     </pre>
     <hr>
-    <h3></h3>
+    <h3>Return Parameters if input is empty</h3>
     <pre><code>
+        function checkUserRoles ($userRoles){
 
+            if(empty($userRoles)){
+                echo "Roles cannot be empty";
+                return;
+            }
+
+            switch($userRoles){
+
+                //Do not excecute when user role is empty
+                case "Admin";
+                    echo "Hello Admin";
+                    break;
+                case "Editor";
+                    echo "Hello Editor";
+                    break;
+                default;
+                    echo "Rol is empty"; //This will be excuded using return if empty
+            }
+            
+        }
+
+        checkUserRoles("Admin");
+        checkUserRoles("");
+        checkUserRoles("Editor");
     </code></pre>
     <h3><i>Result:</i></h3>
     <?php
-        
+        function checkUserRoles ($userRoles){
+
+            if(empty($userRoles)){
+                echo "Roles cannot be empty";
+                return;
+            }
+
+            switch($userRoles){
+
+                //Do not excecute when user role is empty
+                case "Admin";
+                    echo "Hello Admin";
+                    break;
+                case "Editor";
+                    echo "Hello Editor";
+                    break;
+                default;
+                    echo "Rol is empty"; //This will be excuded using return if empty
+            }
+        }
+
+        checkUserRoles("Admin");
+        checkUserRoles("");
+        checkUserRoles("Editor");
     ?>
     
 </body>
