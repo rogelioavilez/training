ab455198624a6d277235ef3ee81b1c07f81f21cb
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-05 21:19:28 -0600

    new file

---
+++ PHPtraining/11-ControlStructure/9-Return.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Return</title>
+</head>
+<body>
+    <h1>Return</h1>
+    <pre>
+        * 
+    </pre>
+    <hr>
+    <h3></h3>
+    <pre><code>
+
+    </code></pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        
+    ?>
+    
+</body>
+</html>
