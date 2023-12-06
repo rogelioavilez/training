9c5c50c063ac3ceaab30ddfe9467f802a9913977
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-05 21:07:37 -0600

    new file

---
+++ PHPtraining/11-ControlStructure/8-Continue.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Continue</title>
+</head>
+<body>
+    <h1>Continue</h1>
+
+    <hr>
+    <pre><code>
+
+    </code></pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        
+    ?>
+    
+</body>
+</html>
