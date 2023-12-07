318856d4e83d5f63bfdd35452e7017ae7bf82d31
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-06 22:03:08 -0600

    new file

---
+++ PHPtraining/12-Strings/4-stringLenght.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Lenght</title>
+</head>
+<body>
+    <h1>String Lenght</h1>
+    <pre>
+        * Get the String Lenght of Variable $name1
+    </pre>
+    <hr>
+    <h3>Example 1:</h3>
+    <pre><code>
+        $name1 = "Rogelio Avilez";
+        //Set Variable example
+        echo strlen($name1);
+
+        echo "<\br>";
+        
+        //UnSet Variable example
+        echo strlen($name2);
+    </code></pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        $name1 = "Rogelio Avilez";
+        //Set Variable example
+        echo strlen($name1);
+
+        echo "<br>";
+
+        //UnSet Variable example
+        echo strlen($name2);
+    ?>
+</body>
+</html>
