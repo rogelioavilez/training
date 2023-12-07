4ed710b38ba2fe4e718d432962805e8e354a840b
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-06 22:36:14 -0600

    new file

---
+++ PHPtraining/12-Strings/6-stringFunctions.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Functions</title>
+</head>
+<body>
+    <h1>String Functions</h1>
+    <pre>
+        * 
+    </pre>
+    <hr>
+    <div>
+        <h3>Example 1:</h3>
+        <p>
+
+        </p>
+        <pre><code>
+
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            
+        ?>
+    </div>
+</body>
+</html>
