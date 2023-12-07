576dde86bf29532dd5ddcba80e0e4d8645b2dc61
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-06 23:34:23 -0600

    new file

---
+++ PHPtraining/13-Arrays/1-DefineAndAccessArray.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Defina and Access Array</title>
+</head>
+<body>
+    <h1>Defina and Access Array</h1>
+    <pre>
+        * 
+    </pre>
+    <hr>
+
+    <!-- Example 1 -->
+    <h3>Title</h3>
+    <pre>
+        - Description
+    </pre>
+    <pre><code>
+
+    </code></pre>
+    <h3><i>Result:</i></h3>
+    <?php
+        
+    ?>
+    
+</body>
+</html>
