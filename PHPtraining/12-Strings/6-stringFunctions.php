c3f49cbc07a5f168d1d89467e0220337bd746e8e
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-06 22:38:39 -0600

    1

--- PHPtraining/12-Strings/6-stringFunctions.php
+++ PHPtraining/12-Strings/6-stringFunctions.php
@@ -8,11 +8,19 @@
 <body>
     <h1>String Functions</h1>
     <pre>
-        * 
+        * Number of Words
+        * Replace String
+        * Reverse String
+        * Remove White spaces
+        * Shuffle Strings
+        * Find Position Case Insensitive
+        * Upper and Lower case
+        * Word Wrap the String
     </pre>
     <hr>
+    <!-- Example 1 -->
     <div>
-        <h3>Example 1:</h3>
+        <h3>Number of Words</h3>
         <p>
 
         </p>
@@ -21,7 +29,7 @@
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
-            
+
         ?>
     </div>
 </body>
