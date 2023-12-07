05e774e4b4954ddf70594ab361d81cf86e214fc6
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-06 22:05:39 -0600

    1.1

--- PHPtraining/12-Strings/4-stringLenght.php
+++ PHPtraining/12-Strings/4-stringLenght.php
@@ -8,10 +8,15 @@
 <body>
     <h1>String Lenght</h1>
     <pre>
-        * Get the String Lenght of Variable $name1
+        * Get the String Lenght of Variable requested.
+        * When String is Unset, this shows 0 as value, no String lenght found.
+        * Blank spaces are counted as part of the String Lenght.
     </pre>
     <hr>
     <h3>Example 1:</h3>
+    <p>
+        Get String Lenght on variable $name1.
+    </p>
     <pre><code>
         $name1 = "Rogelio Avilez";
         //Set Variable example
