9ba9e5d0d9480d1d1d693ac29f454ffa54fca4bd
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-06 21:58:27 -0600

    1.1

--- PHPtraining/12-Strings/3-nowDoc.php
+++ PHPtraining/12-Strings/3-nowDoc.php
@@ -14,7 +14,23 @@
             Write a block of String using NowDoc.
         </p>
         <pre><code>
-            
+            $name = "Rogelio Avilez";
+            $number = 10;
+
+            //Use of single quotes in the Label turns a hereDoc into a nowDoc.
+            echo <<< 'DOC_LABEL'
+            You can write anything inside this
+            "Double Quotes" . Anything
+            'Single Quotes' ... . . . .
+            $name know how to write PHP
+            ($number * 10)
+            New lines and Strings.
+            (true) ? "Some Efect" : "No Efect";
+            <strong> Is this Bold </strong>
+
+            Even the variable substitution will Not work.
+
+            DOC_LABEL;
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
