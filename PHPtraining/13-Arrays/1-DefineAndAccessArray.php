b49be5b634cfb023a44409315aa1705ab4f978ad
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-06 23:45:44 -0600

    1

--- PHPtraining/13-Arrays/1-DefineAndAccessArray.php
+++ PHPtraining/13-Arrays/1-DefineAndAccessArray.php
@@ -8,21 +8,50 @@
 <body>
     <h1>Defina and Access Array</h1>
     <pre>
-        * 
+        * An Array is actually an ordered map. A map is  a type that associates values to keys.
     </pre>
     <hr>
 
     <!-- Example 1 -->
-    <h3>Title</h3>
+    <h3>Learn how to define and use Array</h3>
     <pre>
-        - Description
+        - Define an Array
     </pre>
     <pre><code>
+        //This was the old version of creating arrays:
 
+        $old_array = array("old food", "old drinks");
+        var_dump($old_array);
+
+        echo "<br>";
+
+        //After PHP 5.4 you can replace array {} with []
+
+        $string_array = [
+            "food", "drinks"
+        ];
+
+        var_dump($string_array);
     </code></pre>
     <h3><i>Result:</i></h3>
     <?php
-        
+        /* 
+        This was the old version of creating arrays:
+
+        */
+
+        $old_array = array("old food", "old drinks");
+        var_dump($old_array);
+
+        echo "<br>";
+
+        //After PHP 5.4 you can replace array {} with []
+
+        $string_array = [
+            "food", "drinks"
+        ];
+
+        var_dump($string_array);
     ?>
     
 </body>
