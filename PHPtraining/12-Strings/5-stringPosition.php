4dc07217572b4af0a51216f228595a2a67bfc8f6
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-06 22:18:23 -0600

    new file

--- PHPtraining/12-Strings/5-stringPosition.php
+++ PHPtraining/12-Strings/5-stringPosition.php
@@ -7,6 +7,34 @@
 </head>
 <body>
     <h1>String Position</h1>
-    
+    <pre>
+
+    </pre>
+    <hr>
+    <div>
+        <h3>Example 1:</h3>
+        <p>
+            Find the String position inside another String.
+        </p>
+        <pre><code>
+            $content = "This is a very long content and it is a String";
+
+            $findContentWord = "content";
+
+            $position = strpos($content, $findContentWord);
+
+            echo $position;
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            $content = "This is a very long content and it is a String";
+
+            $findContentWord = "content";
+
+            $position = strpos($content, $findContentWord);
+
+            echo $position;
+        ?>
+    </div>
 </body>
 </html>
