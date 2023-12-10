4e7ed4747a1d2d3c8b555b39dabaabfae2a73a12
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-10 09:57:20 -0600

    1.1

--- PHPtraining/13-Arrays/3-mixedArray.php
+++ PHPtraining/13-Arrays/3-mixedArray.php
@@ -8,18 +8,44 @@
 <body>
     <h1>Mixed Arrays</h1>
     <pre>
-
+    * If index is not specified, PHP will use the increment of the largest previously used integer key.
     </pre>
 
     <hr>
 
     <div>
+        <h3>Add after largest used integer key</h3>
         <pre><code>
+            //Defining arrays while skiping some key elements
+                $arr = [];
+                $arr[0] = 1;
+                $arr[2] = "String";
+                $arr[4] = true;
+                $arr[10] = 14.5;
 
+            //Adding new values to incomplete array
+                $arr[] = "New String 1";
+                $arr[] = "New String 2";
+
+            //Output
+                var_dump($arr);
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
-            
+            //Defining arrays while skiping some key elements
+            $arr = [];
+            $arr[0] = 1;
+            $arr[2] = "String";
+            $arr[4] = true;
+            $arr[10] = 14.5;
+
+            //Adding new values to incomplete array
+            $arr[] = "New String 1";
+            $arr[] = "New String 2";
+
+            //Output
+            var_dump($arr);
+
         ?>
     </div>
 </body>
