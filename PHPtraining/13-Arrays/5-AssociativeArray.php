ecb0aa340e60e50d39c1ee0f103b85fed437fdb4
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-10 10:14:10 -0600

    nfile

---
+++ PHPtraining/13-Arrays/5-AssociativeArray.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Associative Arrays</title>
+</head>
+<body>
+    <h1>Associative Arrays</h1>
+    <pre>
+    * 
+    </pre>
+
+    <hr><div>
+        <h3></h3>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+        
+        ?>
+    </div>
+</body>
+</html>
