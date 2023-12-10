95930c2825f5094c21d1bf0b12bad28994f9fb38
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-10 09:52:25 -0600

    new file

---
+++ PHPtraining/13-Arrays/3-mixedArray.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mixed Arrays</title>
+</head>
+<body>
+    <h1>Mixed Arrays</h1>
+    <pre>
+
+    </pre>
+
+    <hr>
+
+    <div>
+        <pre><code>
+
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            
+        ?>
+    </div>
+</body>
+</html>
