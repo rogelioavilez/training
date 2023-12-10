eca082036bf4abeda6a994f370b86b7592641fec
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-10 08:20:39 -0600

    2

--- PHPtraining/13-Arrays/1-DefineAndAccessArray.php
+++ PHPtraining/13-Arrays/1-DefineAndAccessArray.php
@@ -13,63 +13,83 @@
     <hr>
 
     <!-- Example 1 -->
-    <h3>Learn how to define and use Array</h3>
-    <pre>
-        - To Define an Array use []
-        - Use var_dump in order to print Array contents
-    </pre>
-    <pre><code>
-        //This was the old version of creating arrays:
+    <div>
+        <h3>Learn how to define and use Array</h3>
+        <pre>
+            - To Define an Array use []
+            - Use var_dump in order to print Array contents
+        </pre>
+        <pre><code>
+            //This was the old version of creating arrays:
 
-        $old_array = array("old food", "old drinks");
-        var_dump($old_array);
+            $old_array = array("old food", "old drinks");
+            var_dump($old_array);
 
-        echo "<\br>";
+            echo "<\br>";
 
-        //After PHP 5.4 you can replace array () with []
+            //After PHP 5.4 you can replace array () with []
 
-        $string_array = [
-            "food", "drinks"
-        ];
+            $string_array = [
+                "food", "drinks"
+            ];
 
-        var_dump($string_array);
+            var_dump($string_array);
 
-        echo "<\br>";
+            echo "<\br>";
 
-        $integer_array = [
-            5, 6, 7, 8
-        ];
+            $integer_array = [
+                5, 6, 7, 8
+            ];
 
-        var_dump($integer_array);
-    </code></pre>
-    <h3><i>Result:</i></h3>
-    <?php
-        /* 
-        This was the old version of creating arrays:
+            var_dump($integer_array);
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            /* 
+            This was the old version of creating arrays:
 
-        */
+            */
 
-        $old_array = array("old food", "old drinks");
-        var_dump($old_array);
+            $old_array = array("old food", "old drinks");
+            var_dump($old_array);
 
-        echo "<br>";
+            echo "<br>";
 
-        //After PHP 5.4 you can replace array () with []
+            //After PHP 5.4 you can replace array () with []
 
-        $string_array = [
-            "food", "drinks"
-        ];
+            $string_array = [
+                "food", "drinks"
+            ];
 
-        var_dump($string_array);
+            var_dump($string_array);
 
-        echo "<br>";
+            echo "<br>";
 
-        $integer_array = [
-            5, 6, 7, 8
-        ];
+            $integer_array = [
+                5, 6, 7, 8
+            ];
 
-        var_dump($integer_array);
-    ?>
+            var_dump($integer_array);
+        ?>
+    </div>
+
+    <hr>
+    <!-- Define an Array -->
+    <div>
+        <h3>find references in <a href="https://www.php.net/manual/en">https://www.php.net/manual/en</a></h3>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            $arr = [1, 2, 3];
+
+            //function to print array
+
+            var_dump($arr);
+        ?>
+    </div>
+    
     
 </body>
 </html>
