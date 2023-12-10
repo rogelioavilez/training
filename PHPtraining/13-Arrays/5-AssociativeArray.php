85c9c158456a5d5a502a34a696786d15d71f8845
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-10 10:23:25 -0600

    1.1

--- PHPtraining/13-Arrays/5-AssociativeArray.php
+++ PHPtraining/13-Arrays/5-AssociativeArray.php
@@ -8,9 +8,12 @@
 <body>
     <h1>Associative Arrays</h1>
     <pre>
-    * 
+        * In associative Arrays you specify the Index
+        * index => value
+        * Short notation =
+            => is a special symbol to represent key and value
+        * key => value - Key is the index and Value is the value at the index.
     </pre>
-
     <hr><div>
         <h3></h3>
         <pre><code>
@@ -18,7 +21,30 @@
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
-        
+
+        //indexed Array
+            $arr = ["mon", "tues", "wed","thurs", "fri", "sat", "sun"];
+
+            var_dump($arr);
+
+            echo "<br>";
+
+            foreach($arr as $value){
+                echo $value;
+                echo "<br>";
+            }
+
+        //Associative Array
+            $arr = [0 => "mon", 1 => "tues", 2 => "wed", 3 => "thurs", 4 => "fri", 5 => "sat", 6 => "sun"];
+
+            var_dump($arr);
+
+            echo "<br>";
+
+            foreach($arr as $value){
+                echo $value;
+                echo "<br>";
+            }
         ?>
     </div>
 </body>
