a856fad8b68ed50ea4ec1a4eb443867e5358cc25
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-10 09:29:49 -0600

    new file

---
+++ PHPtraining/13-Arrays/2-forEachLoop.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For Each Loop using Arrays</title>
+</head>
+<body>
+    <h1>For Each Loop using Arrays</h1>
+    <pre>
+    * example of for each template:
+
+        foreach($array as $value){
+            //Code to be executed
+        }
+    </pre>
+
+    <hr>
+    <div>
+        <h3></h3>
+        <pre><code>
+
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            $arr = ["Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"];
+            var_dump($arr);
+
+            $counter = 0;
+
+            foreach($arr as $values){
+                echo "[$counter] => $values => $arr[$counter]";
+
+                $counter++;
+            }
+        ?>
+    </div>
+    
+</body>
+</html>
