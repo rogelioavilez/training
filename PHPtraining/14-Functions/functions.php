3f04fd32c866a7c296e9740d174e985dcb478181
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-11 21:04:34 -0600

    new file

---
+++ PHPtraining/14-Functions/functions.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functions</title>
+</head>
+<body>
+    <h1>Functions</h1>
+    <pre>
+        * 
+    </pre>
+    <hr>
+
+    <!-- Define and Call a Function -->
+    <div>
+        <h3>Define and Call a Function</h3>
+        <pre><code>
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+        ?>
+    </div>
+
+</body>
+</html>
