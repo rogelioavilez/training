54ef89b9a1e67422316bc56cd4183895595285d2
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-11 21:13:43 -0600

    1.1

--- PHPtraining/14-Functions/1-defineAndCallAFunction.php
+++ PHPtraining/14-Functions/1-defineAndCallAFunction.php
@@ -48,7 +48,7 @@
             function printEvenNumbers($limit){
                 for($index = 0; $index <= $limit; $index++){
                     if($index%2 == 0){
-                        echo "Even Number: $index";
+                        echo "Even Number: $index <br>";
                     }
                 }
             }
