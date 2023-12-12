c1ed4ccaf743942c4e440b2625b0506a8e3cb65c
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-11 21:21:46 -0600

    1

--- PHPtraining/14-Functions/2-PassingInputParameters.php
+++ PHPtraining/14-Functions/2-PassingInputParameters.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Passing Input Parameters</title>
 </head>
 <body>
-    <h1></h1>
+    <h1>Passing Input Parameters</h1>
     <pre>
         * 
     </pre>
@@ -14,7 +14,7 @@
 
     
     <div>
-        <h3></h3>
+        <h3>Passing Input Parameters</h3>
         <pre><code>
             
         </code></pre>
