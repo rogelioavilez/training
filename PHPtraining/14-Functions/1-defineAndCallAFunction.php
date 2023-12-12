92ce6a817ee3bf21418ec57d4b1cf7dedfcbb9a0
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-11 21:13:04 -0600

    1

--- PHPtraining/14-Functions/1-defineAndCallAFunction.php
+++ PHPtraining/14-Functions/1-defineAndCallAFunction.php
@@ -3,22 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Functions</title>
+    <title>Define And Call A Function</title>
 </head>
 <body>
-    <h1>Functions</h1>
+    <h1>Define And Call A Function</h1>
     <pre>
-        * 
+        * Basic Syntax on how to write a function on PHP
+
+        function function_name(param1, .... , param_m)
+        {
+            statement_1;
+            statement_2;
+            ......
+            statement_m;
+
+            return return_value;
+        }
     </pre>
     <hr>
 
-    <!-- Define and Call a Function -->
+    
     <div>
-        <h3>Define and Call a Function</h3>
+        <h3>Define and Call the function</h3>
         <pre><code>
+            // Define a function
+
+                function printEvenNumbers($limit){
+                    for($index = 0; $index <= $limit; $index++){
+                        if($index%2 == 0){
+                            echo "Even Number: $index";
+                        }
+                    }
+                }
+
+            //Call the function
+
+                printEvenNumbers(20);
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
+
+        // Define a function
+
+            function printEvenNumbers($limit){
+                for($index = 0; $index <= $limit; $index++){
+                    if($index%2 == 0){
+                        echo "Even Number: $index";
+                    }
+                }
+            }
+
+        //Call the function
+
+            printEvenNumbers(20);
         ?>
     </div>
 
