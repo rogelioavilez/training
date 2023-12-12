4482529bc95e2941dafa4ad2ab1f0543fe951024
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-11 20:10:16 -0600

    new file

---
+++ PHPtraining/13-Arrays/8-sortArray.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sort Array</title>
+</head>
+<body>
+    <h1>Sort Array</h1>
+    <pre>
+        *
+    </pre>
+    <hr>
+
+    <!-- Example 1 -->
+    <div>
+        <h3>Unset Array</h3>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+        
+        ?>
+    </div>
+    
+    
+</body>
+</html>
