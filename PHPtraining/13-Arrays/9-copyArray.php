a950ad3c014e2fc100fceb534e035c9f3e7e0bf0
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-11 20:33:44 -0600

    1

--- PHPtraining/13-Arrays/9-copyArray.php
+++ PHPtraining/13-Arrays/9-copyArray.php
@@ -8,19 +8,23 @@
 <body>
     <h1>Copy Array</h1>
     <pre>
-        * 
+        * Just assign the value of one Array into a new Array assignment.
 
     </pre>
     <hr>
 
-    <!--  -->
+    <!-- Copy Array from one variable to Other -->
     <div>
-        <h3></h3>
+        <h3>Example: Copy Array from one variable to Other</h3>
         <pre><code>
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
+            $arr = [0,1,2,3,4,5];
 
+            $arr1 = $arr;
+
+            var_dump($arr1);
         ?>
     </div>
 
