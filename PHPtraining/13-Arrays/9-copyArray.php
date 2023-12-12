7b90897edc3dc1b3ec5e6799df65a7f617bd7fb5
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-11 20:30:43 -0600

    new file

---
+++ PHPtraining/13-Arrays/9-copyArray.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Copy Array</title>
+</head>
+<body>
+    <h1>Copy Array</h1>
+    <pre>
+        * 
+
+    </pre>
+    <hr>
+
+    <!--  -->
+    <div>
+        <h3></h3>
+        <pre><code>
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+
+        ?>
+    </div>
+
+</body>
+</html>
