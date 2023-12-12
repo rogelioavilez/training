03f6fa1399d69c4c16273305f8ab4a867e573a75
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-11 21:35:39 -0600

    1

--- PHPtraining/14-Functions/2-PassingInputParameters.php
+++ PHPtraining/14-Functions/2-PassingInputParameters.php
@@ -18,28 +18,6 @@
     <div>
         <h3>Passing Input Parameters</h3>
         <pre><code>
-
-        declare(strict_types=1);
-
-        //define function
-            function printOddNumbers(int $limit, $skipNumber){
-
-                for($index = 0; $index <=$limit; $index++){
-
-                    if($index == $skipNumber){
-                        continue;
-                    }
-
-                    if($index%2!=0){
-                        echo "Odd Number: $index";
-                    }
-
-                }
-
-            }
-
-        //Call the function
-            printOddNumbers(20, "3");
             
         </code></pre>
         <h3><i>Result:</i></h3>
