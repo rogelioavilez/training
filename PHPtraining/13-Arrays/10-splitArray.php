c4761a3f49b874682dfabf6fcc1d8d6dcbf68c7f
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-11 20:45:21 -0600

    New file

---
+++ PHPtraining/13-Arrays/10-splitArray.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Split Array</title>
+</head>
+<body>
+    <h1>Split Array</h1>
+    <pre>
+        * 
+
+    </pre>
+    <hr>
+
+    <!-- String to Array -->
+    <div>
+        <h3>Example 1: String to Array</h3>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            $students = "John, Roger, Anna, Jacob, Ruth";
+            $studentArr = explode(",", $students);
+            print_r($studentArr);
+        ?>
+    </div>
+
+
+</body>
+</html>
