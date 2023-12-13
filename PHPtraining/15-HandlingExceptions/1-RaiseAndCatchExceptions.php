29f9b6f23a2e449f8170866842741ecc01f42ae0
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 21:13:39 -0600

    1

--- PHPtraining/15-HandlingExceptions/1-RaiseAndCatchExceptions.php
+++ PHPtraining/15-HandlingExceptions/1-RaiseAndCatchExceptions.php
@@ -8,7 +8,16 @@
 <body>
     <h1>Raise and Cathc Exceptions</h1>
     <pre>
-        * 
+        * example of syntax:
+
+            try{
+                //This is an Error
+
+            } catch (){
+
+                //Jumps to this block
+
+            }
     </pre>
 
     <!-- Raise-Catch a divide by Zero Error -->
@@ -16,10 +25,48 @@
     <div>
         <h3>Raise-Catch a divide by Zero Error</h3>
         <pre><code>
+            function divide($x, $y):int{
+                if($y <=0){
+                    throw new Exception("Divide by Zero Exception might happen");
+                }
 
+                $result = $x/$y;
+                return $result;
+            }            
+
+            try{
+                //This is the Error
+                $result = divide(1,0);
+
+            } catch (Exception $e){
+
+                //Jumps to this block
+                echo 'Caught Exception: ' . $e->getMessage();
+
+            }
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
+
+            function divide($x, $y):int{
+                if($y <=0){
+                    throw new Exception("Divide by Zero Exception might happen");
+                }
+
+                $result = $x/$y;
+                return $result;
+            }            
+
+            try{
+                //This is the Error
+                $result = divide(1,0);
+
+            } catch (Exception $e){
+
+                //Jumps to this block
+                echo 'Caught Exception: ' . $e->getMessage();
+
+            }
         ?>
     </div>
 
