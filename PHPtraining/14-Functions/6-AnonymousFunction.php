e335f63014ab4854de9f53a7e37e583aa01decbc
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 20:09:50 -0600

    new file

---
+++ PHPtraining/14-Functions/6-AnonymousFunction.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anonymous Function</title>
+</head>
+<body>
+    <h1>Anonymous Function</h1>
+    <pre>
+        * 
+    </pre>
+    <!-- Example 1 -->
+    <hr>
+    <div>
+        <h3>Example 1:</h3>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+        
+        ?>
+    </div>
+
+
+</body>
+</html>
