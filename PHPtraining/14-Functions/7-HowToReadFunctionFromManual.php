5f4875834365b6afc28aae54075aeb34df95b6be
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 20:31:16 -0600

    1.1

--- PHPtraining/14-Functions/7-HowToReadFunctionFromManual.php
+++ PHPtraining/14-Functions/7-HowToReadFunctionFromManual.php
@@ -8,18 +8,20 @@
 <body>
     <h1>How to read function from PHP Manual</h1>
     <pre>
-        * 
+        * example on how to read function:
+
+        function_name (parameter_type parameter_name) : return_type
     </pre>
     <!-- Example 1 -->
     <hr>
     <div>
         <h3>Example 1:</h3>
         <pre><code>
-            
+            // in_array (mixed $needle, array $haystack [, bool $strict = FALSE]) : bool
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
-        
+            // in_array (mixed $needle, array $haystack [, bool $strict = FALSE]) : bool
         ?>
     </div>
 
