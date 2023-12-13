35158ec9e38d4cada8113d0cd16b213c7d44b571
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 22:52:33 -0600

    2

--- PHPtraining/16-Files/1-FileDirectories.php
+++ PHPtraining/16-Files/1-FileDirectories.php
@@ -2,8 +2,24 @@
 
     echo "<h1>File Directory</h1>
     <hr>
-    <p>Sample 1: TestFolder1</p>";
+    <h3>Sample 1: get Array of Contents inside \"TestFolder1\" using path</h3>";
 
     $path = "../../files/TestFolder1";
     $result = scandir($path);
     var_dump($result);
+
+    echo "<hr>
+    <h3>Sample 2: delete the dots \" . \" from Array path</h3>";
+
+    $path2 = "../../files/TestFolder1";
+    $result = scandir($path2);
+    
+    foreach($result as $dir){
+
+        if($dir != "." && $dir != ".."){
+
+            echo $dir;
+
+        }
+
+    }
