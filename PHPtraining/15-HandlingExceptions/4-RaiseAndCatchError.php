3ff479f323656d01fabfef4d956b186e35c255db
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 22:06:15 -0600

    1

--- PHPtraining/15-HandlingExceptions/4-RaiseAndCatchError.php
+++ PHPtraining/15-HandlingExceptions/4-RaiseAndCatchError.php
@@ -7,21 +7,18 @@
 </head>
 <body>
     <h1>Raise and Catch Error</h1>
-    <pre>
-        * 
-    </pre>
 
-    <!-- Raise and Exception and Finally Block -->
+    <!-- Raise and Catch Error -->
     <hr>
     <div>
-        <h3>Raise and Exception and Finally Block</h3>
+        <h3>Raise and Catch Error</h3>
         <pre><code>
             function custom_error($errno, $errstr){
                 echo $errstr;
             }
 
             set_error_handler('custom_error');
-            echo (5/0);
+            echo (5 / 0);
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
@@ -30,7 +27,7 @@
             }
 
             set_error_handler('custom_error');
-            echo (5/0);
+            echo (5 / 0);
         ?>
     </div>
     
