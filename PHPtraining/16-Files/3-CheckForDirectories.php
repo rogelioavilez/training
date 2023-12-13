647774aaebac5f0d4f20cb018f9f8d1aa314cd75
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 23:17:08 -0600

    new files

---
+++ PHPtraining/16-Files/3-CheckForDirectories.php
@@ -0,0 +1,18 @@
+<h1>Check for Directories inside other Directories</h1>
+
+<?php
+
+    $path = "../../files/TestFolder1";
+    $directory = scandir($path);
+    
+    $directory = array_diff($directory, ['.', '..']);
+
+    foreach($directory as $dir){
+
+        if(is_dir($path . "/" .$dir)){
+
+            echo $dir . "<br>";
+
+        }
+
+    }
