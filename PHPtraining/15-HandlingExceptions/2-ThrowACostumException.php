d3a2cf590c21001d962dcd42a0a824d98c5f2020
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 21:31:04 -0600

    2

--- PHPtraining/15-HandlingExceptions/2-ThrowACostumException.php
+++ PHPtraining/15-HandlingExceptions/2-ThrowACostumException.php
@@ -33,6 +33,48 @@
         ?>
     </div>
 
+        <!-- Raise-Catch a divide by Zero Error -->
+        <hr>
+    <div>
+        <h3>Raise-Catch a divide by Zero Error</h3>
+        <pre><code>
+            function divide($x, $y):int{
+                if($y <=0){
+                    throw new Exception("Divide by Zero Exception might happen");
+                }
+
+                $result = $x/$y;
+                return $result;
+            }            
+
+            try{
+                //This is the Error
+                $result = divide(1,0);
+
+            } catch (Exception $e){
+
+                //Jumps to this block
+                echo 'Caught Exception: ' . $e->getMessage();
+
+            }
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            function divide($x, $y):int{
+                if($y <=0){
+                    throw new Exception("Divide by Zero Exception might happen");
+                }
+
+                $result = $x/$y;
+                return $result;
+            }   
+            
+            set_exception_handler('divide');
+
+            throw new Exception ("Exception Happened!");
+        ?>
+    </div>
+
     
 
 </body>
