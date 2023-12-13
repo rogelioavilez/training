41e5a9c2fb43960c3c100e2bdc2ffc230f037fd4
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 23:10:58 -0600

    new file

---
+++ PHPtraining/16-Files/2-CheckForFileInDir.php
@@ -0,0 +1,18 @@
+<h1>Check for file in Directory</h1>
+
+<?php
+
+    $path = "../../files/TestFolder1";
+    $directory = scandir($path);
+    
+    $directory = array_diff($directory, ['.', '..']);
+
+    foreach($directory as $dir){
+
+        if(is_file($path . "/" .$dir)){
+
+            echo $dir . "<br>";
+
+        }
+
+    }
