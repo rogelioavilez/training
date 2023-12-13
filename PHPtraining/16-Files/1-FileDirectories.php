a6830e03cb31df4f8a61fda7c1958284d37a70d4
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 22:42:54 -0600

    new files

--- PHPtraining/16-Files/1-FileDirectories.php
+++ PHPtraining/16-Files/1-FileDirectories.php
@@ -1,3 +1,9 @@
 <?php
 
-    echo "<h1>File Directory</h1>";
+    echo "<h1>File Directory</h1>
+    <hr>
+    <p>Sample 1: TestFolder1</p>";
+
+    $path = "TestFolder1";
+    $result = scandir($path);
+    var_dump($result);
