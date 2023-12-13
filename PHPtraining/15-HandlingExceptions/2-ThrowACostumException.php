66ce47b790cf90113c5430a001b11de9f549cc91
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 21:19:47 -0600

    new file

---
+++ PHPtraining/15-HandlingExceptions/2-ThrowACostumException.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Throw a costum Exception</title>
+</head>
+<body>
+    <h1>Throw a costum Exception</h1>
+    <pre>
+        * 
+    </pre>
+
+    <!-- 1 -->
+    <hr>
+    <div>
+        <h3>1</h3>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+            
+        ?>
+    </div>
+
+</body>
+</html>
