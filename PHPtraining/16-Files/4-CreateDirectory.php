1025460c49d6a160e33e6b1d768aaf6f2ef0de54
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 23:49:42 -0600

    1.4

--- PHPtraining/16-Files/4-CreateDirectory.php
+++ PHPtraining/16-Files/4-CreateDirectory.php
@@ -2,10 +2,10 @@
 
 <?php
 
-    $path = "../../files/TestFolder1";
+    $path = "../../files/TestFolder1/";
     $result = scandir($path);
     var_dump($result);
 
-    if(!file_exists($result . "/" . "TestFolder3")){
+    if(!file_exists($result . "TestFolder3")){
         mkdir("TestFolder3");
     }
