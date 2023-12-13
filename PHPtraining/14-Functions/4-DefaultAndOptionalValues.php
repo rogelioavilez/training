069806d6a648dbb62bafada59bc409c5246436e0
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 19:47:12 -0600

    new file

---
+++ PHPtraining/14-Functions/4-DefaultAndOptionalValues.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Default and Optional Values</title>
+</head>
+<body>
+    <h1>Default and Optional Values</h1>
+    <pre>
+        * Example:
+
+
+        
+    </pre>
+    <!-- Example 1 -->
+    <hr>
+    <div>
+        <h3>Example 1: .</h3>
+        <pre><code>
+            
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+        
+        ?>
+    </div>
+
+
+</body>
+</html>
