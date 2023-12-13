bc9bab930b7b40ba695347b73f6bc0874135375d
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 21:55:24 -0600

    new file

--- PHPtraining/15-HandlingExceptions/3-FinallyBlock.php
+++ PHPtraining/15-HandlingExceptions/3-FinallyBlock.php
@@ -7,17 +7,40 @@
 </head>
 <body>
     <h1>Finally Block</h1>
+    <pre>
+        * Finally block will always get excecuted, this will take place before the exceptions get thrown
+    </pre>
 
-    <!-- 1 -->
+    <!-- Raise and Exception and Finally Block -->
     <hr>
     <div>
-        <h3>1</h3>
+        <h3>Raise and Exception and Finally Block</h3>
         <pre><code>
-            
+            function custom_exception($except){
+                echo 'Caught Exception: ' . $except->getMessage();
+            }
+
+            set_exception_handler("custom_exception");
+
+            try{
+                throw new Exception("Exception Raised!");
+            } finally{
+                echo "This line gets excecuted before Exception Always!";
+            }
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
-        
+            function custom_exception($except){
+                echo 'Caught Exception: ' . $except->getMessage();
+            }
+
+            set_exception_handler("custom_exception");
+
+            try{
+                throw new Exception("Exception Raised!");
+            } finally{
+                echo "This line gets excecuted before Exception Always!";
+            }
         ?>
     </div>
     
