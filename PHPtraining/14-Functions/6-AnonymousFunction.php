fd98bdb4f5c505b8434638e3f1b33ed70f46dc30
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 20:17:25 -0600

    1

--- PHPtraining/14-Functions/6-AnonymousFunction.php
+++ PHPtraining/14-Functions/6-AnonymousFunction.php
@@ -8,7 +8,20 @@
 <body>
     <h1>Anonymous Function</h1>
     <pre>
-        * 
+        * Anonymous Function does not have a function name
+        * they are also called Closures
+
+        example->
+
+        *define the function:
+
+            $function_variable = function (parameters_here){
+                return: Something;
+            }
+
+        * to call function:
+
+            echo $function_variable(parameters_here);
     </pre>
     <!-- Example 1 -->
     <hr>
@@ -19,7 +32,13 @@
         </code></pre>
         <h3><i>Result:</i></h3>
         <?php
-        
+
+            $Anon_function = function ($a, $b){
+                $addNumbers = $a + $b;
+                return $addNumbers;
+            };
+
+            echo $Anon_function(4,7);
         ?>
     </div>
 
