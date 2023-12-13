dbeac1fe1ce4fb7a7a1f51650c6b9a2df767aa4a
rogelioavilez <rogelioavilez@oneqrid.com>
2023-12-12 21:05:42 -0600

    new file

---
+++ PHPtraining/15-HandlingExceptions/1-RaiseAndCatchExceptions.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raise and Catch Exceptions</title>
+</head>
+<body>
+    <h1>Raise and Cathc Exceptions</h1>
+    <pre>
+        * 
+    </pre>
+
+    <!-- Raise-Catch a divide by Zero Error -->
+    <hr>
+    <div>
+        <h3>Raise-Catch a divide by Zero Error</h3>
+        <pre><code>
+
+        </code></pre>
+        <h3><i>Result:</i></h3>
+        <?php
+        ?>
+    </div>
+
+</body>
+</html>
